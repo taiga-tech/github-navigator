05a735b5f2fca1ce20c82c27b33dd7efd3b58ec8
taiga-tech <taiga.dev.n@gmail.com>
2025-07-15 22:42:25 +0900

    fix: reorder import statements in Toaster component

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -2,8 +2,8 @@
 
 import { useTheme } from 'next-themes'
 
-import { Toaster as Sonner } from 'sonner'
 import { type ToasterProps } from 'sonner'
+import { Toaster as Sonner } from 'sonner'
 
 const Toaster = ({ ...props }: ToasterProps) => {
     const { theme = 'system' } = useTheme()
